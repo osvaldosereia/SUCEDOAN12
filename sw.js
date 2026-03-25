3448d2217e377b5f43d9d706649d797686ae67af
OSSJU <Junior.sereia@hotmail.com>
2026-03-25 19:41:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Mudamos para v2 para forçar o telemóvel dos clientes a atualizar o sistema!
-const CACHE_NAME = 'dona-antonia-app-v3'; 
+const CACHE_NAME = 'dona-antonia-app-v4'; 
 const urlsToCache = [
   './index.html',
   './manifest.json'
