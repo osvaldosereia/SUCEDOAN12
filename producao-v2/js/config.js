355ebb9f940606e29098f27e28abf5331879c1e6
OSSJU <Junior.sereia@hotmail.com>
2026-07-24 12:27:52 -0400

    adiciona configuracao segura do admin v2

---
+++ producao-v2/js/config.js
@@ -0,0 +1,10 @@
+export const DEFAULT_CONFIG = Object.freeze({
+  firebaseUrl: 'https://cedar-chemist-310801-default-rtdb.firebaseio.com',
+  productsNode: 'produtos',
+  writeMode: false,
+  pageSize: 50,
+});
+
+export const STORAGE_KEYS = Object.freeze({
+  config: 'da_admin_v2_config',
+});
