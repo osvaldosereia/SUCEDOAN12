64446a03d500b267ead7fc590dd790cd661758b8
OSSJU <Junior.sereia@hotmail.com>
2026-07-27 11:20:35 -0400

    Corrige rotina de datas do checkout

--- app-next/src/checkout.js
+++ app-next/src/checkout.js
@@ -24,7 +24,7 @@ function easterSunday(year) {
   const f = Math.floor((b + 8) / 25);
   const g = Math.floor((b - f + 1) / 3);
   const h = (19 * a + b - d - g + 15) % 30;
-  const i = Math.floor(b / 4);
+  const i = Math.floor(c / 4);
   const k = c % 4;
   const l = (32 + 2 * e + 2 * i - h - k) % 7;
   const m = Math.floor((a + 11 * h + 22 * l) / 451);
