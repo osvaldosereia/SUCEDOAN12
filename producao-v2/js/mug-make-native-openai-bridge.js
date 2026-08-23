5d64524969743f8a63bc565a1a4a43bd290e191b
OSSJU <Junior.sereia@hotmail.com>
2026-08-23 17:57:19 -0300

    canecas: carrega biblioteca compacta de comandos

--- producao-v2/js/mug-make-native-openai-bridge.js
+++ producao-v2/js/mug-make-native-openai-bridge.js
@@ -1,8 +1,9 @@
 import './mug-studio-gallery.js?admin_build=20260823-mug-v7-gallery';
 import './mug-personalizer-v7.js?admin_build=20260823-canecas-studio-v7-2';
 import './mug-command-library-v1.js?admin_build=20260823-canecas-command-library-v1';
+import './mug-command-library-compact-v2.js?admin_build=20260823-canecas-command-compact-v2';
 
-const BUILD = '20260823-canecas-studio-v7-2-command-library';
+const BUILD = '20260823-canecas-studio-v7-2-command-library-v2';
 
 function install() {
   window.__daMugStudioV7Loader = BUILD;
