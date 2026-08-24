4286c6b5df04902c7f4aae77262bddb57854ea72
OSSJU <Junior.sereia@hotmail.com>
2026-08-24 16:03:49 -0300

    Caneca Print: adiciona atalho de impressão silenciosa

---
+++ caneca-print/abrir-caneca-print.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+set "URL=https://donaantonia.com.br/caneca-print/?kiosk=1"
+set "CHROME=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
+if exist "%CHROME%" goto abrir
+set "CHROME=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
+if exist "%CHROME%" goto abrir
+set "CHROME=%LocalAppData%\Google\Chrome\Application\chrome.exe"
+if exist "%CHROME%" goto abrir
+
+echo Google Chrome nao foi encontrado.
+echo Instale o Chrome ou ajuste o caminho neste arquivo.
+pause
+exit /b 1
+
+:abrir
+start "Caneca Print" "%CHROME%" --app="%URL%" --kiosk-printing --start-maximized --disable-session-crashed-bubble
+exit /b 0
