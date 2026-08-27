081f23d4bdffc391e9f01d99385551582decc28c
OSSJU <Junior.sereia@hotmail.com>
2026-08-26 21:03:19 -0300

    Canecas V17: invalidar cache do módulo de mídia corrigido

--- app-next/src/image-performance.js
+++ app-next/src/image-performance.js
@@ -1,4 +1,4 @@
-import './product-media.js?v=20260821-canecas-v2';
+import './product-media.js?v=20260826-canecas-clean-v17';
 
 const TRANSPARENT_PIXEL = 'data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22 width=%221%22 height=%221%22/%3E';
 const PRELOAD_MARGIN = 1100;
