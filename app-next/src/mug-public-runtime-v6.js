442a228df9c2428bf02fe2c439f4b03fc8ccfdee
OSSJU <Junior.sereia@hotmail.com>
2026-08-27 14:46:17 -0300

    Força recarga do recovery público de canecas

--- app-next/src/mug-public-runtime-v6.js
+++ app-next/src/mug-public-runtime-v6.js
@@ -1,4 +1,4 @@
-const BUILD = '20260827-site-mug-runtime-v10-public-contract-result';
+const BUILD = '20260827-site-mug-runtime-v11-public-recovery-cache';
 let featurePromise = null;
 
 function isProductRoute() {
