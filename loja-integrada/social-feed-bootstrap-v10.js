1a96ca8a9d82359b2eac001a09ee7ac6fc2d77e4
OSSJU <Junior.sereia@hotmail.com>
2026-09-02 12:43:13 -0300

    chore: publicar social feed v10 mobile compacto

--- loja-integrada/social-feed-bootstrap-v10.js
+++ loja-integrada/social-feed-bootstrap-v10.js
@@ -1,8 +1,8 @@
 (function(){
 "use strict";
-if(window.CFSOCIAL10BOOT==="20260902-3")return;
-window.CFSOCIAL10BOOT="20260902-3";
-var v="20260902-3";
+if(window.CFSOCIAL10BOOT==="20260902-4")return;
+window.CFSOCIAL10BOOT="20260902-4";
+var v="20260902-4";
 var l=document.createElement("link");
 l.rel="stylesheet";
 l.href="https://donaantonia.com.br/loja-integrada/social-feed-v10.css?v="+v;
