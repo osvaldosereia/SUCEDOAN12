169433dd26562df5f925dc0b92dea7218c0400f8
OSSJU <Junior.sereia@hotmail.com>
2026-09-02 11:31:56 -0300

    chore: bootstrap remoto do social feed v10

---
+++ loja-integrada/social-feed-bootstrap-v10.js
@@ -0,0 +1,15 @@
+(function(){
+"use strict";
+if(window.CFSOCIAL10BOOT)return;
+window.CFSOCIAL10BOOT="20260902-1";
+var v="20260902-1";
+var l=document.createElement("link");
+l.rel="stylesheet";
+l.href="https://donaantonia.com.br/loja-integrada/social-feed-v10.css?v="+v;
+document.head.appendChild(l);
+var s=document.createElement("script");
+s.src="https://donaantonia.com.br/loja-integrada/social-feed-v10.js?v="+v;
+s.async=true;
+s.onerror=function(){console.error("[CanecaFácil] Falha ao carregar Social Feed V10")};
+document.head.appendChild(s);
+})();
