1021f61cf1612cbaccb52ee874b819517c2375e1
OSSJU <Junior.sereia@hotmail.com>
2026-09-02 11:57:42 -0300

    chore: atualizar cache do feed social corrigido

--- loja-integrada/social-feed-bootstrap-v10.js
+++ loja-integrada/social-feed-bootstrap-v10.js
@@ -1,8 +1,8 @@
 (function(){
 "use strict";
-if(window.CFSOCIAL10BOOT)return;
-window.CFSOCIAL10BOOT="20260902-1";
-var v="20260902-1";
+if(window.CFSOCIAL10BOOT==="20260902-2")return;
+window.CFSOCIAL10BOOT="20260902-2";
+var v="20260902-2";
 var l=document.createElement("link");
 l.rel="stylesheet";
 l.href="https://donaantonia.com.br/loja-integrada/social-feed-v10.css?v="+v;
