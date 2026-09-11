3ca5855de14a866fd0483622ef8a5579fed96214
OSSJU <Junior.sereia@hotmail.com>
2026-09-11 09:23:17 -0400

    chore: bump unified storefront build

--- catalogo/config.js
+++ catalogo/config.js
@@ -1,4 +1,4 @@
 window.DA_CATALOGO_CONFIG=Object.freeze({
   api:'https://ssbesxgaijknwsjbsbcz.supabase.co/functions/v1/mini-catalog-v1',
-  build:'20260911-vitrine-visual-06'
+  build:'20260911-vitrine-unificada-08'
 });
