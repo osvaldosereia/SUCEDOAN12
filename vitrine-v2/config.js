0ec288eebaa2a8c7c8cfd807b569c69b09e73af2
OSSJU <Junior.sereia@hotmail.com>
2026-09-11 15:27:28 -0400

    feat: configurar Vitrine V2

---
+++ vitrine-v2/config.js
@@ -0,0 +1,7 @@
+export const CONFIG=Object.freeze({
+  supabaseUrl:'https://ssbesxgaijknwsjbsbcz.supabase.co',
+  supabasePublishableKey:'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
+  edgeFunction:'storefront-v2',
+  whatsappNumber:'5565998150975',
+  build:'20260911-vitrine-v2-1'
+});
