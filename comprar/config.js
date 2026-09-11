5d96d284ec9f2a0b1ad8b863895b4eb763c342c2
OSSJU <Junior.sereia@hotmail.com>
2026-09-11 00:25:25 -0400

    chore(chat): point storefront to lightweight chat API

--- comprar/config.js
+++ comprar/config.js
@@ -1,6 +1,7 @@
 window.DA_SHOPPING_ROOM_CONFIG=Object.freeze({
-  api:'https://ssbesxgaijknwsjbsbcz.supabase.co/functions/v1/shopping-room-v1',
+  api:'https://ssbesxgaijknwsjbsbcz.supabase.co/functions/v1/shopping-chat-v1',
+  legacyApi:'https://ssbesxgaijknwsjbsbcz.supabase.co/functions/v1/shopping-room-v1',
   salesApi:'https://ssbesxgaijknwsjbsbcz.supabase.co/functions/v1/shopping-room-sales-v1',
   whatsappFallback:'https://wa.me/556584491018',
-  build:'20260907-shopping-room-03'
+  build:'20260911-light-chat-01'
 });
