24d435d498891b7ea700cf24669ffe729cbb4adc
OSSJU <Junior.sereia@hotmail.com>
2026-09-11 16:52:41 -0400

    feat: add Vitrine V3 config

---
+++ vitrine-v3/config.js
@@ -0,0 +1,8 @@
+export const CONFIG=Object.freeze({
+  supabaseUrl:'https://ssbesxgaijknwsjbsbcz.supabase.co',
+  supabasePublishableKey:'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
+  catalogFunction:'catalog-v3',
+  orderFunction:'storefront-v2',
+  whatsappNumber:'5565998150975',
+  build:'20260911-vitrine-v3-1'
+});
