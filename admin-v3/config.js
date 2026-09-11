8e9fe29864fade33ea662dea3a3056ae6b5a2c6d
OSSJU <Junior.sereia@hotmail.com>
2026-09-11 17:01:47 -0400

    feat: point Admin V3 at simple V3 API

--- admin-v3/config.js
+++ admin-v3/config.js
@@ -1,7 +1,8 @@
-window.DA_ADMIN_V3_CONFIG = Object.freeze({
-  supabaseUrl: 'https://ssbesxgaijknwsjbsbcz.supabase.co',
-  supabasePublishableKey: 'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
-  edgeFunction: 'admin-ops-v1',
-  countAppUrl: '../contagem-v2/',
-  build: '20260907-admin-v3-01'
+export const CONFIG=Object.freeze({
+  supabaseUrl:'https://ssbesxgaijknwsjbsbcz.supabase.co',
+  supabasePublishableKey:'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
+  adminFunction:'admin-v3-api',
+  storefrontUrl:'../vitrine-v3/',
+  countAppUrl:'../contagem/',
+  build:'20260911-admin-v3-1'
 });
