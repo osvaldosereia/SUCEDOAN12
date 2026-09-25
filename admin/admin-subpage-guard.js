ac5e8029dcbfaf9e6a710be8f19d2b3190501a6b
OSSJU <Junior.sereia@hotmail.com>
2026-09-24 23:59:46 -0400

    chore: remove retired general admin surface

--- admin/admin-subpage-guard.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-  'use strict';
-  try{
-    const auth=JSON.parse(localStorage.getItem('da_admin_auth')||'null');
-    if(auth?.access_token||auth?.refresh_token)return;
-  }catch{}
-  const current=location.pathname.split('/').pop()||'index.html';
-  location.replace(`./?next=${encodeURIComponent(current)}`);
-})();
--- admin/image-runtime-config.js
+++
@@ -1,5 +0,0 @@
-export const CONFIG=Object.freeze({
-  supabaseUrl:'https://ssbesxgaijknwsjbsbcz.supabase.co',
-  supabasePublishableKey:'sb_publishable_tFXHtH0HCXZepVtwgKElIg_DxS76Gu8',
-  build:'20260916-admin-images-1'
-});
